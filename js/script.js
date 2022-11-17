86ffbb0e709b819e4d8e29bd850893ca69c305d4
admin <marcial.dev@gmail.com>
2022-11-17 18:08:17 -0300

    Implmented responsive Header, Hero, Second, Third, Fourth and Textonly sections

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* MENU BUTTON */
+const $menuButton = document.querySelector(".menu-button")
+const $mainNav = document.querySelector(".main-nav")
+
+$menuButton.addEventListener("click", () => {
+   $mainNav.classList.toggle("-active")
+   $menuButton.classList.toggle("-active")
+})
+
